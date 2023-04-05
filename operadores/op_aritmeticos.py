5cd10afe5526d5ebf534e7f85ef33dcea09f159e
alejandro <alejandro.tellez@gmail.com>
2023-04-04 18:39:02 -0600

    Operadores

---
+++ operadores/op_aritmeticos.py
@@ -0,0 +1,28 @@
+# suma y resta ( + y - )
+suma= 12 + 5
+resta = 12 - 5
+
+# multiplicación y división ( * y / )
+multi = 12 * 5
+division = 12 / 6 # devuelve un dato float
+
+# potencia (exponente) (**)
+exponente = 12 ** 5
+
+# división baja (//)
+division_baja = 12 // 5 # devuelve entero redondeado hacia abajo
+
+# resto o módulo
+modulo = 12 % 5
+
+# Determinar el tipo de dato
+tipo_de_dato = type(division)
+
+print(f"Suma: {suma}")
+print(f"Resta: {resta}")
+print(f"Multiplicación: {multi}")
+print(f"División: {division}")
+print(f"Tipo: {tipo_de_dato}")
+print(f"Exponente: {exponente}")
+print(f"División baja: {division_baja}")
+print(f"Módulo: {modulo}")
---
+++ operadores/op_comparacion.py
@@ -0,0 +1,24 @@
+igual_que = 5 == 6
+
+distinto_de = 5 != 6
+
+mayor_que = 5 > 6
+
+menor_que = 5 < 6
+
+mayor_o_igual = 5 >= 6
+
+menor_o_igual = 5 <= 6
+
+# calculos combinados
+
+a = 5
+b = 10
+c = 20
+comparacion = a + b < c
+print(comparacion)
+
+# comparar cadenas
+contraseña_almacenada = "12345"
+contarseña_escrita = "12345"
+print(contraseña_almacenada == contarseña_escrita)
